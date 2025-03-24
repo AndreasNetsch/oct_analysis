e38edeb18a8575bf36caa57c35d55d236abfc6f5
Andreas Netsch <xs8747@partner.kit.edu>
2025-03-24 16:23:27 +0100

    Added function find_substratum

--- src/oct_analysis/image_processing.py
+++ src/oct_analysis/image_processing.py
@@ -4,6 +4,7 @@ Image processing functions for oct_analysis
 
 import cv2
 import os
+import numpy as np
 
 
 def read_tiff(file_path):
@@ -41,3 +42,73 @@ def read_tiff(file_path):
         return img
     except Exception as e:
         raise ValueError(f"Error reading TIFF file: {str(e)}")
+
+
+def find_substratum (img, start_x, y_max, roi_width, scan_height, box_width):
+    """
+    Find the substratum in an image.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        The image as a numpy array
+    start_x : int
+        The x-coordinate of the starting point
+    y_max : int
+        The maximum y-coordinate
+    roi_width : int
+        The width of the region of interest
+    scan_height : int
+        The height of the scan area
+    box_width : int
+        The width of the box to scan
+
+    Returns
+    -------
+    numpy.ndarray
+        The image as a numpy array
+
+    Raises
+    ------
+    FileNotFoundError
+        If the file does not exist
+    ValueError
+        If the file could not be read as an image
+    """
+    img = img[:, ::-1, :]
+    slices, h, w = img.shape
+    
+    for slice_idx, img in enumerate(img):
+        maxSum = 0
+        memBot = 0
+        # Find the bottom of the membrane in the first slice
+        for i in range(y_max):
+            roi = img[i, start_x:start_x+roi_width]
+            sum_val = np.mean(roi)
+            if sum_val > maxSum:
+                maxSum = sum_val
+                memBot = i
+            memBot1 = memBot
+
+    # Process each slice
+        for x in range(start_x, w, box_width):
+            memBot = memBot1
+            for y in range(memBot - scan_height, memBot + scan_height, 1):
+                # Ensure 'y' is within bounds for the image height
+                if y < 0:
+                    y = 0
+                elif y >= img.shape[0]:  # img.shape[0] is the height of the image
+                    y = img.shape[0] - 1
+                roi = img[y, x:x+roi_width]
+                sum_val = np.mean(roi)
+                if sum_val > maxSum:
+                    maxSum = sum_val
+                    memBot = y
+                if start_x == 0:
+                    memBot1 = memBot
+            maxSum = 0
+            if memBot > 0:
+                img[:memBot, x:x+box_width] = 0  # Set area to black
+        #print(slice_idx)
+    img = img[:, ::-1, :]
+    return img
