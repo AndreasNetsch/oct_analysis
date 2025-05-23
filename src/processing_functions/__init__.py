5327b11e061fffdd9444872f95856390aefa2d66
Jonas <dope.lemon@posteo.eu>
2025-05-23 21:39:49 +0200

    changed folder name oct_analysis-->processing_functions, changed import of own modules to absolute import

--- src/processing_functions/__init__.py
+++ src/processing_functions/__init__.py
@@ -4,7 +4,7 @@ oct_analysis - A library for image processing functions
 
 __version__ = "1.0.0"
 
-from .image_processing import (
+from src.processing_functions.image_processing import (
     read_tiff, save_tiff, select_tiff_folder, save_results_to_csv,
     convert_to_8bit, find_substratum, find_two_point_line,
     find_max_zero, untilt, binary_mask, voxel_count,
